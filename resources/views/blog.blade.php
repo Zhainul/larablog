54e8fc20ca46586f5a8035fa0a93213b5bc37a7c
Zhainul <zainulanwar244@gmail.com>
2025-06-17 08:37:41 +0700

    refactor: use model and laravel tinker

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,10 +6,10 @@
             <h3 class="text-xl font-bold text-gray-900">{{ $blog['title'] }}</h3>
         </a>
         <div class="text-sm text-gray-500">
-            <a href="#">{{ $blog['author'] }}</a> | {{ $blog['date'] }}
+            <a href="#">{{ $blog['author'] }}</a> | {{ date('d/m/Y H:i:s', strtotime($blog['created_at'])) }}
         </div>
         <p class="font-light my-4">{{ Str::limit($blog['description'], 100, '...') }}</p>
-        <a href="blog/{{ $blog['slug'] }}" class="rounded-full border border-blue-950 bg-white text-blue-950 text-xs px-2 hover:text-white hover:bg-blue-950">Readmore &raquo;</a>
+        <a href="/blog/{{ $blog['slug'] }}" class="rounded-full border border-blue-950 bg-white text-blue-950 text-xs px-2 hover:text-white hover:bg-blue-950">Readmore &raquo;</a>
     </article>
     @endforeach
 </x-layout>
